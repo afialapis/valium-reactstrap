504a64cdb82e10e860e49573d2952f22dc3ec037
afialapis <devel@afialapis.com>
2019-12-05 22:50:12 +0100

    simpler naming components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 
-import VFormRS          from './components/VFormRS'
-import VInputTextRS     from './components/inputs/VInputTextRS'
-import VInputEmailRS    from './components/inputs/VInputEmailRS'
-import VInputUrlRS      from './components/inputs/VInputUrlRS'
-import VInputPasswordRS from './components/inputs/VInputPasswordRS'
-import VInputTextAreaRS from './components/inputs/VInputTextAreaRS'
-import VInputNumberRS   from './components/inputs/VInputNumberRS'
-import VInputDateRS     from './components/inputs/VInputDateRS'
-import VInputTimeRS     from './components/inputs/VInputTimeRS'
-import VInputColorRS    from './components/inputs/VInputColorRS'
-import VInputCheckboxRS from './components/inputs/VInputCheckboxRS'
-import VInputSelectRS   from './components/inputs/VInputSelectRS'
+import VFormRS                from './components/VFormRS'
+import VInputTextRS           from './components/inputs/VInputTextRS'
+import VInputEmailRS          from './components/inputs/VInputEmailRS'
+import VInputUrlRS            from './components/inputs/VInputUrlRS'
+import VInputPasswordRS       from './components/inputs/VInputPasswordRS'
+import VInputTextAreaRS       from './components/inputs/VInputTextAreaRS'
+import VInputNumberRS         from './components/inputs/VInputNumberRS'
+import VInputDateRS           from './components/inputs/VInputDateRS'
+import VInputTimeRS           from './components/inputs/VInputTimeRS'
+import VInputColorRS          from './components/inputs/VInputColorRS'
+import VInputCheckboxRS       from './components/inputs/VInputCheckboxRS'
+import VInputSelectRS         from './components/inputs/VInputSelectRS'
 import VInputSelectMultipleRS from './components/inputs/VInputSelectMultipleRS'
 import VInputSelectSearchRS   from './components/inputs/VInputSelectSearchRS'
 import VInputFileRS           from './components/inputs/VInputFileRS'
  
-export {VFormRS, VInputTextRS, VInputEmailRS, VInputUrlRS, VInputPasswordRS, VInputTextAreaRS, 
-        VInputNumberRS, VInputDateRS, VInputTimeRS, VInputColorRS, VInputCheckboxRS, VInputSelectRS, VInputSelectMultipleRS, VInputSelectSearchRS, VInputFileRS}
+export {VFormRS as VForm,
+        VInputTextRS as VInputText,
+        VInputEmailRS as VInputEmail,
+        VInputUrlRS as VInputUrl,
+        VInputPasswordRS as VInputPassword,
+        VInputTextAreaRS as VInputTextArea,
+        VInputNumberRS as VInputNumber,
+        VInputDateRS as VInputDate,
+        VInputTimeRS as VInputTime,
+        VInputColorRS as VInputColor,
+        VInputCheckboxRS as VInputCheckbox,
+        VInputSelectRS as VInputSelect,
+        VInputSelectMultipleRS as VInputSelectMultiple,
+        VInputSelectSearchRS as VInputSelectSearch,
+        VInputFileRS as VInputFile,
+       }
 
-
+       
