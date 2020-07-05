e772cd8a03cb54147115e0e1437e249333f7186f
afialapis <devel@afialapis.com>
2020-07-05 18:07:41 +0200

    no logs

--- src/components/inputs/common/useInnerValue.js
+++ src/components/inputs/common/useInnerValue.js
@@ -9,14 +9,11 @@ const useInnerValue = (value, defaultValue, onChange, onConfirm) => {
   const [confirmedValue, setConfirmedValue]= useState(undefined)
 
   useEffect(() => {
-    console.log(`Effect ${value} ${defaultValue}`)
     const [vprops, nvalue]= valueOrDef(value, defaultValue)
     if (vprops!=vProps) {
-      console.log(`Effect vProps => ${JSON.stringify(vProps)}`)
       setVProps(vprops)
     }
     if (nvalue!=innerValue) {
-      console.log(`Effect innerValue => ${innerValue}`)
       setInnerValue(nvalue)
       setConfirmedValue(nvalue)
     }
@@ -31,7 +28,6 @@ const useInnerValue = (value, defaultValue, onChange, onConfirm) => {
   const handleChange = useCallback(
     (event) => {
       const value= event.target.value
-      console.log(`inner handleChange(${innerValue} => ${value})`)
       if (value!=innerValue) {
         setInnerValue(value)
 
@@ -45,7 +41,6 @@ const useInnerValue = (value, defaultValue, onChange, onConfirm) => {
 
   const handleOnFocus= useCallback(
     () => {
-      console.log(`inner handleFocus(set confirmed: ${confirmedValue})`)
       setConfirmedValue(innerValue)
     },
     [innerValue]
@@ -53,7 +48,6 @@ const useInnerValue = (value, defaultValue, onChange, onConfirm) => {
 
   const handleOnBlur = useCallback(
     () => {
-      console.log(`inner handleBlur(${innerValue} -last ${confirmedValue}-)`)
       if (onConfirm!=undefined) {
         if (confirmedValue!=innerValue) {
           setConfirmedValue(innerValue)
