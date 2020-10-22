82a267cadc9d63be6ec5287ed67c842cfb6e0e2b
afialapis <devel@afialapis.com>
2020-10-22 19:22:38 +0200

    arroy keys support on number inputs

--- demo/src/inputs/DemoInputNumber.js
+++ demo/src/inputs/DemoInputNumber.js
@@ -3,12 +3,10 @@ import {VInputNumber} from '../../../src'
 
 const DemoInputNumber = ({showAddon, keepHeight}) => {
 
-  const [pills, setPills]= useState(2)
+  const [pills, setPills]= useState(2.5)
   const [weight, setWeight]= useState(105.876)
   const [size, setSize]= useState(1.44)
-
-  console.log(pills)
-
+  
   return (
     <>
       <VInputNumber
@@ -17,6 +15,7 @@ const DemoInputNumber = ({showAddon, keepHeight}) => {
               onChange    = {(v) => setPills(v)}
               required    = {false}
               decimals    = {1}
+              step        = {2}
               checkValue  = {(v) => v>=6}
               label       = {"How many pills per dose would you like?"}
               description = {"Some >=6 integer. Decimals allowed but invalid (decimals=1)."}
@@ -32,6 +31,7 @@ const DemoInputNumber = ({showAddon, keepHeight}) => {
               onChange    = {(v) => setWeight(v)}
               required    = {true}
               decimals    = {3}
+              step        = {1.111}
               label       = {"Hmm... sounds like too much pills. How much do you weight?"}
               description = {"Some float (max 3 decimals, decimals = 3)."}
               keepHeight  = {keepHeight}
--- src/VInputNumber.js
+++ src/VInputNumber.js
@@ -10,15 +10,14 @@ import {withValium} from './valium/withValium'
 
 const _VInputNumber = (props) => {
   const {id, name, inputRef, placeholder, 
-         readOnly, required, min, max, /*step,*/
+         readOnly, required, min, max, step, decimals,
          valid, autocomplete, onChange,
          inputStyle}= props
 
 
   const [innerValue, setInnerValue, controlled] = useInnerValue(props)
 
-  const handleChange = useCallback((event) => {
-    const value= event.target.value
+  const updValue = useCallback((value) => {
     setInnerValue(value)
     if (onChange!=undefined) {
       let v= value
@@ -29,6 +28,31 @@ const _VInputNumber = (props) => {
     }
 
   }, [setInnerValue, onChange])
+
+  const handleChange = useCallback((event) => {
+    const value= event.target.value
+    updValue(value)
+
+  }, [updValue])
+
+  const handleKeyDown = useCallback((event) => {
+    if (event.key=='ArrowUp' || event.key=='ArrowDown') {
+      event.preventDefault()
+      let curValue = 0
+      try {
+        curValue= parseFloat(innerValue)
+      } catch(_) {}
+      const incr= step!=undefined
+                  ? step
+                  : 1.00
+      const factor = event.key=='ArrowUp' ? 1 : -1
+      let nValue= curValue + (factor*incr)
+      if (decimals!=undefined && nValue.toFixed!=undefined) {
+        nValue = nValue.toFixed(decimals)
+      }
+      updValue(nValue)
+    }
+  }, [updValue, innerValue, step, decimals])
 
 
   const valueProps= controlled 
@@ -48,11 +72,12 @@ const _VInputNumber = (props) => {
             required    = {required}
             max         = {max}
             min         = {min}
-            //step        = {step || undefined}
+            step        = {step}
             valid       = {valid}
             invalid     = {! valid}
             autoComplete= {autocomplete}
             style       = {inputStyle} 
+            onKeyDown   = {(ev) => handleKeyDown(ev)}
             {...valueProps}
     />
   )
@@ -68,7 +93,7 @@ VInputNumber.propTypes = {
   placeholder         : PropTypes.string,
   max                 : PropTypes.number,
   min                 : PropTypes.number,
-  //step                : PropTypes.number,
+  step                : PropTypes.number,
   //pattern             : PropTypes.string,
   autocomplete        : PropTypes.oneOf(["on", "off"]),
 }
